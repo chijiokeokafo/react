477ed1928b9c031a57746ec9647add3c4a42f817
Chijioke Okafo <cokafo@innocean.ca>
2017-03-17 14:45:34 -0400

    Adding two way data binding to page, also adding component lifecycle functions and a mount/unmount home component button

--- basics/src/app/components/Home.js
+++ basics/src/app/components/Home.js
@@ -5,13 +5,63 @@ export class Home extends React.Component {
 		super();
 		this.state = {
 			age: props.initialAge, 
-			status: 0
+			status: 0, 
+			homeLink: props.initialLinkName
 		};
+		setTimeout(() => {
+			this.setState({
+				status: 1
+			});
+		}, 3000);
+		console.log("Constructor");
 	}
+
+	componentWillMount() {
+		console.log("component will mount");
+	}
+
+	componentDidMount() {
+		console.log("component did mount");
+	}
+
+	componentWillReceiveProps(nextProps) {
+		console.log("component will receive props", nextProps)
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("Should Component update", nextProps, nextState);
+		// if(nextState.status === 1) {
+		// 	return false;
+		// }
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log("Component will update", nextProps, nextState);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log("Component did update", prevProps, prevState);
+	}
+
+	componentWillUnmount() {
+		console.log("Component will unmount");
+	}
+
 	onMakeOlder() {
 		this.setState({
 			age: this.state.age + 3
 		});
+	}
+
+	onChangeLink() {
+		this.props.changeLink(this.state.homeLink);
+	}
+
+	onHandleChange(event) {
+		this.setState({
+			homeLink: event.target.value
+		})
 	}
 
 	render() {
@@ -22,6 +72,14 @@ export class Home extends React.Component {
 				<p>Status: {this.state.status}</p> 
 				<hr/>
 				<button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older</button>
+				<hr />
+				<button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+				<hr/>
+				<input type="text" value={this.state.homeLink}
+					onChange={(event) => this.onHandleChange(event)} />
+				<br />
+				<br />
+				<button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
 			</div>
 
 		);
@@ -31,4 +89,6 @@ export class Home extends React.Component {
 Home.propTypes = {
 	name: React.PropTypes.string, 
 	initialAge: React.PropTypes.number,
-}
+	greet: React.PropTypes.func, 
+	initialLinkName: React.PropTypes.string
+};
